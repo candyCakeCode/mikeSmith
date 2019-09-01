7c7303fb4608e141054914f1f1b8f9da24f48766
candyCakeCode <52096722+candyCakeCode@users.noreply.github.com>
2019-09-01 02:22:08 -0500

    Add files via upload

---
+++ sortingMikeSmith/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortingMikeSmith
+{
+    class Program
+    {
+                  public static void Main(String[] args)
+            {
+                
+                   string name1 = "mike1234smith";
+                   string name2 = "mike3smith";
+                   string name3 = "mike93smith";
+
+            Console.WriteLine(name2 + " " + name3 + " " + name1);
+            Console.ReadKey();
+        }
+        }
+    }
+    
